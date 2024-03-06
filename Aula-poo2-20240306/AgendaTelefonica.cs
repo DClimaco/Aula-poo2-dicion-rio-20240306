226cdd08b7f1b496798230197bf98f41919b2369
Daniel Carvalho Climaco <111577285+DClimaco@users.noreply.github.com>
2024-03-06 08:25:51 -0300

    Add files via upload

---
+++ Aula-poo2-20240306/AgendaTelefonica.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_poo2_20240306
+{
+    internal class AgendaTelefonica
+    {
+        public string proprietario { get; set; }
+
+        public Dictionary<string, Contato> dicionario { get; set; }
+
+        public AgendaTelefonica(string proprietario)
+        {
+            dicionario = new Dictionary<string, Contato>();
+            this.proprietario = proprietario;
+        }
+
+        public void Inserir(string nome, string telefone)
+        {
+            //Contato c = new Contato(nome, telefone);
+            dicionario.Add(nome, new Contato(nome, telefone));
+        }
+
+        public Contato Buscar(string nome)
+        {
+            return dicionario[nome];
+        }
+
+        public void Remover(string nome)
+        {
+            Contato c = Buscar(nome);
+            if (dicionario.Remove(nome))
+            {
+                Console.WriteLine(nome + " foi removido");
+            }
+            else
+            {
+                Console.WriteLine("Não existe este contato");
+            }
+        }
+
+        public int Tamanho()
+        {
+            return dicionario.Count;
+        }
+    }
+}
+
---
+++ Aula-poo2-20240306/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_poo2_20240306
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AgendaTelefonica a = new AgendaTelefonica("Daniel");
+
+            a.Inserir("Daniel", "00009999");
+            Console.WriteLine(a.Tamanho());
+            Console.WriteLine(a.Buscar("Daniel"));
+            a.Inserir("Victor", "99990000");
+            Console.WriteLine(a.Tamanho());
+            a.Remover("Victor");
+            Console.WriteLine(a.Tamanho());
+
+
+        }
+    }
+}
